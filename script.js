996c01231fd22de99c412a08e4c637f7905a591c
Ana Luiza Câmara <alvc@cesar.school>
2026-09-14 09:37:30 -0300

    Cria estrutura HTML/CSS inicial e exibe primeira cena de teste.

---
+++ script.js
@@ -0,0 +1,8 @@
+const cena = {
+    id: 1,
+    filme: "Harry Potter e a Pedra Filosofal",
+    dialogo: "Você é...",
+    resposta: "um bruxo, Harry."
+};
+
+document.getElementById("dialogo").textContent = cena.dialogo;
